4d1b19ce66ffc5a0d8297b139ca42549ea4a9611
Nahir Martinez <nahirmartinez09@gmail.com>
2023-06-02 13:15:19 -0300

    modificaciones visuales

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
-const USUARIO_NOMBRE_REGISTRADO = "Javier";
-const USUARIO_CONTRASENIA_REGISTRADO = "12345";
-const USUARIO_NOMBRE_REGISTRADO_2 = "Mimi";
-const USUARIO_CONTRASENIA_REGISTRADO_2 = "abcde"
+const inputUsuario = document.querySelector ("#usuario-nombre");
+const inputContrasenia = document.querySelector("#usuario-contrasenia");
+const pResultado = document.querySelector ("#resultado");
 
+const usuarioNombRegist = ["Carlos", "mini", "Pepita"];
+const usuarioContraRegist = ["Carlos1.", "123", "abc"];
+const USUARIO_NO_VERIFICADO = false;
+const USUARIO_VERIFICADO = true;
 
-let usuarioNombreIngresado = prompt("Ingrese su nombre de usuario");
-let usuarioContraseñaIngresada = prompt("Ingrese su contraseña");
+let usuarioNombreIngresado = "def nombre";
+let usuarioContraseniaIngresada = "def contrasenia";
 
-if ((usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO && usuarioContraseñaIngresada == USUARIO_CONTRASENIA_REGISTRADO) || (usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO_2 && usuarioContraseñaIngresada == USUARIO_CONTRASENIA_REGISTRADO_2)) {
+//let usuarioNombreIngresado = prompt("Ingrese su nombre de usuario");
+//let usuarioContraseñaIngresada = prompt("Ingrese su contraseña");
+
+/*if ((usuarioNombreIngresado == usuarioNombRegist && usuarioContraRegist == USUARIO_CONTRASENIA_REGISTRADO) || (usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO_2 && usuarioContraseñaIngresada == USUARIO_CONTRASENIA_REGISTRADO_2)) {
     document.write("Sesion iniciada correctamente");
 }
 else {
     document.write("Usuario o contraseña incorrecta");
+}*/
+
+function enviarInfo(){
+    let estadoVerificacion = USUARIO_NO_VERIFICADO;
+    usuarioNombreIngresado = inputUsuario.value;
+    usuarioContraseniaIngresada = inputContrasenia.value;   
+
+    for (let i = 0; i < usuarioNombRegist.length; i++) {
+        if ( ( usuarioContraseniaIngresada == usuarioContraRegist[i])  && (usuarioNombreIngresado == usuarioNombRegist[i])) {
+            estadoVerificacion = USUARIO_VERIFICADO;
+            i = usuarioNombRegist.length;
+        }
+    }
+
+    if (estadoVerificacion) {
+        pResultado.style.color = "lime";
+        pResultado.innerHTML = "Usuario y contraseña correctos "+ usuarioNombreIngresado;
+    }else{
+        pResultado.innerHTML = "Usuario y contraseña incorrectos";
+        pResultado.style.color = "red";
+    }
 }
